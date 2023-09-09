fc5c3e9927c1e840460c838b354ab43fc9ab7b99
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2023-09-09 12:42:38 +0530

    Create PageBar.js

---
+++ PageBar.js
@@ -0,0 +1,36 @@
+let color = "#ff0"; 
+let height = "5";
+let shadow = "10px";
+let shadowColor = "#f00";
+let borderRadius = "10";
+
+function init(color,height,shadow,shadowColor,border){
+  let style = document.createElement('style');
+  style.textContent=`
+  .bar{
+  height:${height}px;
+  width:var(--width);
+  background:${color};
+  filter:drop-shadow(0 0 ${shadow}px ${shadowColor});
+  position:fixed;
+  top:0;
+  left:0;
+  border-radius:${border}px;
+  z-index:10000;
+}`;
+  let bar = document.createElement('div');
+  bar.classList.add('bar');
+  bar.style="--width:0;";
+  document.body.appendChild(style);
+  document.body.appendChild(bar);
+}
+init(color,height,shadow,shadowColor,borderRadius);
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+var bar = document.querySelector('.bar');
+
+function setScrollVar() {
+  const htmlElement = document.documentElement;
+  const percentOfScreenHeightScrolled =((htmlElement.scrollTop ) / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+  bar.style.setProperty("--width",percentOfScreenHeightScrolled + '%');
+}
